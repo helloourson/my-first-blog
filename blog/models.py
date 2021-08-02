289ca3a456131b9aca65851afdfeb25fe32d9821
Urs Gerhard <helloourson@hotmail.com>
2021-08-02 21:56:40 +0200

    Meine Django-Grils-app, erster Commit

---
+++ blog/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+
+# Create your models here.
+# Blogpost-Model
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # ForeignKey definiert Verknüpfung/ Beziehung zu einen anderen Model
+    title = models.CharField(max_length=200)  # Texfeld mit limitierten Anzahl von Zeichen
+    text = models.TextField()  # Texfeld ohne Grössenbeschränkung
+    created_date = models.DateTimeField(default=timezone.now)  # Feld für einen Zeitpunkt Datum und Uhrzeit
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+# from django.conf import settings
+# from django.db import models
+# from django.utils import timezone
+#
+#
+# class Post(models.Model):
+#     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     title = models.CharField(max_length=200)
+#     text = models.TextField()
+#     created_date = models.DateTimeField(default=timezone.now)
+#     published_date = models.DateTimeField(blank=True, null=True)
+#
+#     def publish(self):
+#         self.published_date = timezone.now()
+#         self.save()
+#
+#     def __str__(self):
+#         return self.title
