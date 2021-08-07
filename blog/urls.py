d122108b0c813611b02e073ca6172634b2283232
Urs Gerhard <helloourson@hotmail.com>
2021-08-07 16:22:27 +0200

    Addes viwes to create/ edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
 
 """ Wie du siehst, fügen wir nun eine view mit dem Namen post_list zur Root-URL hinzu.
@@ -12,7 +14,7 @@ Domain-Namen (z.B. http://127.0.0.1:8000/), der im vollständigen Pfad voransteh
 Dieses Muster sagt Django also, dass views.post_list das gewünschte Ziel ist, wenn jemand
 deine Website über 'http://127.0.0.1:8000/' aufruft. """
 
-# Der letzte Teil name='post_list' ist der Name der URL, der genutzt wird, um die View zu
-# identifizieren. Er kann identisch mit dem Namen der View sein, aber es kann auch ein komplett
-# anderer sein. Wir werden später die Namen der URLs im Projekt benutzen. Daher ist es wichtig, jede
-# URL in der App zu benennen. Wir sollten außerdem versuchen, eindeutige und einfach zu merkende URL-Namen zu wählen.
+""" Der letzte Teil name='post_list' ist der Name der URL, der genutzt wird, um die View zu
+identifizieren. Er kann identisch mit dem Namen der View sein, aber es kann auch ein komplett
+anderer sein. Wir werden später die Namen der URLs im Projekt benutzen. Daher ist es wichtig, jede
+URL in der App zu benennen. Wir sollten außerdem versuchen, eindeutige und einfach zu merkende URL-Namen zu wählen."""
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 from django.shortcuts import get_object_or_404
+from .forms import PostForm
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -15,4 +17,36 @@ def post_list(request):
 #Detail Blog ansehen
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'blog/post_detail.html', {'post':post})
+
+#Formular für neuen Blog
+def post_new(request):
+    if request.method == "POST": #Wenn
+        form = PostForm(request.POST) #PostForm nimmt die ausgefüllten Daten im Forumlar auf
+        if form.is_valid() :
+            post = form.save(commit=False) #Model hier noch nicht speichern
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save() #Formular abspeichern
+            # Um zum neu erzeugten Detail-Blog zu gehen, wird post_detail view aufgerufen
+            return redirect('post_detail', pk=post.pk)
+
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form':form})
+
+#Formular für Bearbeitung von bestehenden Blogs
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False) #Model hier noch nicht speichern
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save() #Formular abspeichern
+            # Um zum neu erzeugten Detail-Blog zu gehen, wird post_detail view aufgerufen
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form':form})
